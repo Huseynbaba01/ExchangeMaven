f9cb99cd2b499fdbb3335efee7734568843665eb
Huseyn <huseynbabarzaguliyev@gmail.com>
2024-02-11 21:27:31 +0100

    ViewBinding implementation

--- app/src/main/java/com/codinghuseyn/exchangemaven/presentation/fragment/ExchangeFragment.kt
+++ app/src/main/java/com/codinghuseyn/exchangemaven/presentation/fragment/ExchangeFragment.kt
@@ -1,7 +1,12 @@
 package com.codinghuseyn.exchangemaven.presentation.fragment
 
 import androidx.fragment.app.Fragment
+import com.codinghuseyn.exchangemaven.databinding.FragmentExchangeBinding
 
 class ExchangeFragment : Fragment() {
+    private var _exchangeBinding: FragmentExchangeBinding? = null
+    val exchangeBinding
+        get() = _exchangeBinding!!
+
 
 }
